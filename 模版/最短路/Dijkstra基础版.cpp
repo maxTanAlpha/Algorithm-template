ddc1ddecd1447d1e9bdfea0ad7987d99665c5893
MYL <734025623@qq.com>
2020-02-26 19:01:41 +0800

    Second Commit

--- 模版/最短路/Dijkstra基础版.cpp
+++ 模版/最短路/Dijkstra基础版.cpp
@@ -7,27 +7,27 @@ using namespace std;
 const int maxn = 1e5, inf = 0x7f7f7f;
 int mmap[maxn][maxn], dis[maxn], visit[maxn]; // dis数组存的为起始点与每个点的最短距离,visit数组存的为0或者1，1表示已经走过这个点
 int n;
-void dijkstra(int start) {
-    memset(visit, 0, sizeof(visit));//初始化为0，表示开始都没走过
-    int pos = 1, min, sum = 0;
-    for (int i = 1; i <= n; i++) {
-        dis[i] = mmap[start][i]; //从起始点到第i个点的距离
-    }
-    visit[start] = 1;
+int dijkstra(int start, int end) { //从start到end的最短路
+    int minn = inf;
+    memset(visit, 0, sizeof(visit));
+    for (int i = 1; i <= n; i++)
+        dis[i] = mmap[start][i];//从起始点到第i个点的距离
     dis[start] = 0;
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j <= n; j++) {
-            min = inf;
-            if (!visit[i] && dis[j] < min) {
-                min = dis[j];
-                pos = j;
+    visit[start] = 1;
+    while (!visit[end]) {
+        minn = inf;
+        visit[start] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (!visit[i] && dis[i] > dis[start]+mmap[start][i]) {
+                dis[i] = dis[start]+mmap[start][i];
             }
         }
-        visit[pos] = 1;//表示这个点已经走过
-        for (int j = 1; j <= n; j++) {
-            if (!visit[j] && dis[j] > dis[pos] + mmap[pos][j]) {
-                dis[j] = dis[pos]+mmap[pos][j];
+        for (int i = 1; i <= n; i++) {
+            if(!visit[i] && minn >= dis[i]) {
+                minn = dis[i];
+                start = i;
             }
         }
     }
+    return dis[end];
 }
